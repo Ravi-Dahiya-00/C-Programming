d59cc2dc20ec807baeceb45c8a474987949b5687
RAVI-DAHIYA--00 <ravidahiya9350@gmail.com>
2024-12-12 01:22:25 +0530

    started programming in C language

---
+++ Basics of C Language/1_basics.c
@@ -0,0 +1,18 @@
+// cd "C:\MY Coding\C Programming\Basics of C Language"
+// gcc 1_basics.c
+// .\a
+
+
+
+// link section
+#include <stdio.h>
+#include <conio.h>     
+
+
+int main()
+{
+    printf("hello world");
+    printf("continue on same line");
+    printf("\n");
+    printf("this is for new line");
+}
