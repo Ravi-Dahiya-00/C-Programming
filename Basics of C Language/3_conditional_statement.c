4e362a09965048771520c6101b5379f27be538cc
RAVI-DAHIYA--00 <ravidahiya9350@gmail.com>
2024-12-20 16:57:23 +0530

    started 3_conditional_statement in C

---
+++ Basics of C Language/3_conditional_statement.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    // even odd finding 
+
+    int x;
+    printf("enter a number you want to check odd or even :");
+    scanf("%d",&x);
+    return 0;
+}
--- Basics of C Language/tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-tional_part=given_float_number-converted_float;
-
-    // printf("fractional part of the given number is : %f",fractional_part);
