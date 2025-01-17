0e798cc67a1ab11c26f9f5599b069dd63c912d9c
RAVI-DAHIYA--00 <ravidahiya9350@gmail.com>
2025-01-17 19:14:59 +0530

    operators in C

--- Basics of C Language/testing.c
+++ Basics of C Language/testing.c
@@ -1,41 +1,10 @@
 #include <stdio.h>
 int main(){
 
- int prime_number,checking_number=0;
-    printf("\n Enter a number to check prime or not : ");
-    scanf("%d",&prime_number);
-
-    if (prime_number<=1)
-    {
-            printf("this number is not prime");
-    }
-
-    
-    if (prime_number==2)
-    {
-            printf("this number is prime");
-    }
-    for (int i=2;i<prime_number;i++)
-    {
-       if (prime_number%i==0 )
-        {             
-            checking_number=checking_number+1;
-            break;
-
-        }
-        if (checking_number==0)
-        {
-            printf("this number is prime");
-        }
-        else
-        {
-            printf("this number is not prime");
-
-        }
-        
-    }
-
-
+ int c,i=10;
+ double b=5.6;
+c=i+b;
+  printf("%d",c);
 
 return 0;
 }
