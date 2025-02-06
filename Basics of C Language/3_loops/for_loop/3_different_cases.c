23b23b82df29b98aba5bde70715d5160fd2e4f6a
RAVI-DAHIYA--00 <ravidahiya9350@gmail.com>
2025-02-06 15:44:59 +0530

    some different cases of for and while loops

--- Basics of C Language/3_loops/for_loop/3_different_cases.c
+++ Basics of C Language/3_loops/for_loop/3_different_cases.c
@@ -1,34 +1,33 @@
-
 #include <stdio.h>
 int main()
 {
-
-   printf("heloo");
+    
+   printf("hello");
     int i=1;
     for(;i<=10;i++);
     {
         printf("%d\n",i);
         i++;
     }
-    printf("heloo");
+    printf("hello");
 
-// output --> heloo 11 heloo 
-
-
+// output --> hello 11 hello 
 
 
 
 
 
 
-printf("heloo");
+
+
+printf("hello");
     int i1=1;
     for(;i1<=10;);
     {
         printf("%d\n",i1);
         i1++;
     }
-    printf("heloo");
+    printf("hello");
 
 
 // output --> heloo           infinite loop
@@ -41,15 +40,14 @@ printf("heloo");
 
 
 
-    printf("heloo");
+    printf("hello");
     int i2=1;
     while(i2<=10);
     {
         printf("%d\n",i2);
         i2++;
     }
-    printf("heloo");
+    printf("hello");
 
-// output --> heloo           infinite loop
-
+// output --> hello           infinite loop
 }
