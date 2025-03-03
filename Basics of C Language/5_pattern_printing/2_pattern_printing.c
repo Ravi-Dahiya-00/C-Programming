5f1c25cba4ad6c7e7d3a661f4df47a5362ed6b7b
RAVI-DAHIYA--00 <ravidahiya9350@gmail.com>
2025-03-04 02:02:35 +0530

    learning pattern printing

--- Basics of C Language/5_pattern_printing/2_pattern_printing.c
+++ Basics of C Language/5_pattern_printing/2_pattern_printing.c
@@ -4,16 +4,98 @@ int main()
 
     // star triangle mast pattern
 
-printf("star triangle mast pattern");
+printf("star triangle mast pattern\n");
 
 for (int i=1;i<=5;i++)
 {
+    for (int j=1;j<=5-i;j++)
+    {
+        printf("  ");
+    }
+    for (int k=1;k<=i;k++)
+    {
+        printf("* ");
+    }
     
     printf("\n");
 }
 
 
 
+
+
+// rhombus pattern printing question
+printf("rhombus pattern printing\n");
+
+for (int i=1;i<=5;i++)
+{
+    for (int j=5;j>=i;j--)
+    {
+        printf(" ");
+    }
+    for(int k=1;k<=5;k++)
+    {
+        printf("*");
+    }
+    for (int q=5;q>=i;q--)
+    {
+        printf(" ");
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
+// reverse alphabet triangle 
+
+printf("reverse alphabet triangle\n");
+
+for (int i=1;i<=5;i++)
+{
+    int a=64;
+    for (int j=5;j>=i;j--)
+    {
+        printf(" ");
+    }
+    for (int k=1;k<=i;k++)
+    {
+        a++;
+        printf("%c",a);
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+// star pyramid 
+for (int i=1;i<=5;i++)
+{
+    for(int j=1;j<=(5-i);j++)
+    {
+        printf(" ");
+    }
+    for (int k=1;k<=i;k++)
+    {
+        printf("*");
+    }
+    for (int j=2;j<=i;j++)
+    {
+        printf("*");
+    }
+    printf("\n");
+}
 
 
 
